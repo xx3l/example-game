d4f0bcb2ac6fcd65d21bdf4da5ee6696ae9b999b
volard <kirillzhilenkov@gmail.com>
2024-01-05 04:33:31 +0800

    feat: add sql setup script

---
+++ src/Database/create_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    name string,
+    password string,
+    xp integer,
+    hp integer,
+    maxHp integer,
+    x integer,
+    y integer,
+    powerAttack integer,
+    inventorySize integer
+);
