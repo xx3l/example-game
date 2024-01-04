74c9775ea3d09ae2bdb11b6e6fa1339340966233
volard <kirillzhilenkov@gmail.com>
2024-01-05 04:32:03 +0800

    refactor: move index.php and use css + js

--- public/index.php
+++ public/index.php
@@ -1,4 +1,16 @@
+<?php declare(strict_types=1); ?>
+
+<style>
+    <?php require_once "./css/main.css"; ?>
+</style>
+<script>
+    <?php require_once "./js/control.js" ?>
+</script>
+
 <?php
+use App\Game;
+
+require '../vendor/autoload.php';
 
 $game = new Game();
 $game->start();
